bbb29d4b92d4a20436d747446414e0c9187866f2
jeanimal <jeanimal@gmail.com>
2024-07-13 22:32:04 -0500

    merge back to main

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+import pandas as pd
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+    df = pd.DataFrame({'Name': [name]})
+    print(df)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
