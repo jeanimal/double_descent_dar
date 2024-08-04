7f4f21be5a50a15ed035b0ac39ecb366a16da236
jeanimal <jeanimal@gmail.com>
2024-08-04 11:41:31 -0500

    Changed to function in terms of train_rows rather than train_size

--- double_descent_dar/sample_eval.py
+++ double_descent_dar/sample_eval.py
@@ -51,12 +51,25 @@ def split_and_calc_metric(X, y, test_size, model, metric_func, random_state: Opt
     metric_test = metric_func(y_test, model.predict(X_test))
     return {'train':metric_train, 'test':metric_test}
 
+# Combine the above two functions.
+def split_sample_and_calc_metric(X, y, train_rows, num_sampled_columns, model, metric_func, random_state: Optional[int] = None):
+    train_size = train_rows / X.shape[0]
+    print(f'using train_size {train_size}')
+    replace = True
+    X_subset = X.sample(n=num_sampled_columns, random_state=random_state, replace=replace, axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_subset, y, train_size=train_size, random_state=random_state)
+    model.fit(X_train, y_train)
+    metric_train = metric_func(y_train, model.predict(X_train))
+    metric_test = metric_func(y_test, model.predict(X_test))
+    return {'train':metric_train, 'test':metric_test}
+
 def sample_and_calc_metric(X, y, num_sampled_rows, num_sampled_columns, test_size, model, metric_func, replace, random_state: Optional[int] = None) -> tuple[pd.DataFrame, pd.DataFrame]:
     X_sub, y_sub = sample_rows_and_cols(X, y, num_sampled_rows, num_sampled_columns, replace=replace, random_state=random_state)
     metric_tuple = split_and_calc_metric(X_sub, y_sub, test_size, model, metric_func, random_state=random_state)
     return metric_tuple
 
-def sample_dataframe(X, y, rows_per_sample, cols_per_sample, num_samples, rng: Optional[np.random.RandomState] = None):
+def sample_dataframes(X, y, rows_per_sample, cols_per_sample, num_samples, rng: Optional[np.random.RandomState] = None):
     if X.shape[0] != y.shape[0]:
         raise ValueError(f"X had and y must have the same number of rows but had {X.shape[0]} and {y.shape[0]}.")
 
@@ -67,7 +80,6 @@ def sample_dataframe(X, y, rows_per_sample, cols_per_sample, num_samples, rng: O
     row_indices = rng.choice(total_rows, size=(num_samples, rows_per_sample), replace=True)
     col_indices = rng.choice(total_cols, size=(num_samples, cols_per_sample), replace=True)
 
-    sampled_X = np.array([X.iloc[row_idx, col_idx].values for row_idx, col_idx in zip(row_indices, col_indices)])
-    sampled_y = np.array([y.iloc[row_idx].values for row_idx in row_indices])
-    # out, outY = sample_dataframe(X, y, rows_per_sample=3, cols_per_sample=4, num_samples=5, rng=rng)
-    return sampled_X, sampled_y
+    sampled_X = [X.iloc[row_idx, col_idx].values for row_idx, col_idx in zip(row_indices, col_indices)]
+    sampled_y = [y.iloc[row_idx].values for row_idx in row_indices]
+    return zip(sampled_X, sampled_y)
